fd45dd3621f2e8ad5dcd727a5504ffb7f26dbbf2
jeffrey Hackford and Kyle Wolfson <jeffrey@>
2016-06-27 13:28:09 -0700

    add passing espresso UI test

--- app/src/androidTest/java/com/epicodus/myrestaurants/MainActivityInstrumentationTest.java
+++ app/src/androidTest/java/com/epicodus/myrestaurants/MainActivityInstrumentationTest.java
@@ -5,6 +5,7 @@ import android.support.test.rule.ActivityTestRule;
 import org.junit.Rule;
 import org.junit.Test;
 
+import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.action.ViewActions.typeText;
@@ -27,5 +28,12 @@ public class MainActivityInstrumentationTest {
         onView(withId(R.id.locationEditText)).perform(typeText("Portland"))
                 .check(matches(withText("Portland")));
     }
-
+    @Test
+        public void locationIsSentToRestaurantActivity() {
+            String location = "Portland";
+            onView(withId(R.id.locationEditText)).perform(typeText(location));
+            onView(withId(R.id.findRestaurantsButton)).perform(click());
+            onView(withId(R.id.locationTextView)).check(matches
+                (withText("Here are all the restaurants near: " + location)));
+    }
 }
